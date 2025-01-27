5fe6182456e528b4b3a12612da9fdefada93164d
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-27 15:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,9 +19,10 @@ class Student extends Person {
 }
 
 class Teacher extends Person {
+	teach() {
 	console.log(`${this.name} is teaching`);
 }
-
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
